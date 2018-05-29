d5f771b1bd2da161dc26a566b6ab943a2155439f
Devin Mounts and Thad Donaghue <thad@>
2018-05-29 11:01:15 -0700

    add initial html and jscript for cups to ml conversion.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Convert cups to milliliters
+// 1 cup = 236.588 ml
+var cups = parseInt(prompt("How many cups to convert?"));
+
+var conversion = function(cups) {
+  return cups * 236.588;
+}
+
+alert(cups + " cups is equal to " + conversion(cups) + "milliliters.");
+
+// Convert teaspoons to tablespoons
+
+
+// Convert pints to quarts
+
+
+// Convert quarts to liters
